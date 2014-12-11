dd1d7b298c86beb64664fde86dbd279b2b8b3629
Alex Benson <longroadhomeproductions@gmail.com>
2014-12-10 20:14:05 -0600

    added mobile menu, ready for presentation

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alexbenson on 7/30/14.
+ */
+$(document).ready(function() {
+
+
+    $('#trigger-overlay, .overlay').click(function() {
+        $('.overlay').toggleClass("open");
+        console.log('overlay');
+    });
+
+    var parentHeight = $('.parent').height();
+    var childHeight = $('.child').height();
+    $('.child').css('margin-top', (parentHeight - childHeight) / 2);
+});
